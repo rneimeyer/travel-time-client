515ed185c28f73ed2c6b0d4da5d8f069942d2bb5
Rebecca Neimeyer <rebeccaneimeyer@RebeccasMacBook.hsd1.ca.comcast.net>
2022-03-28 15:08:44 -0700

    created navbar

---
+++ src/Header/Header.js
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Navbar bg="white" expand="md" fixed="top">
+        <Container>
+          <Navbar.Brand className="brand">Travel Time</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Link className="navbar-links" to="/">
+                Home
+              </Link>
+              <Link className="navbar-links" to="/all-trips">
+                Trips
+              </Link>
+              <Link className="navbar-links" to="/about">
+                About
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
