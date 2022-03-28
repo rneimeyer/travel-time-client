9f82907e4d34cdbf6fde38a424401b8f8f3b62e3
Rebecca Neimeyer <rebeccaneimeyer@RebeccasMacBook.hsd1.ca.comcast.net>
2022-03-28 15:08:14 -0700

    set up main routes and imported components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Home from "./Home/Home";
+import AllTrips from "./AllTrips/AllTrips";
+import About from "./About/About";
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Navigate to="/" />} />
+          <Route path="/all-trips" element={<AllTrips />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+      <Footer />
     </div>
   );
 }
