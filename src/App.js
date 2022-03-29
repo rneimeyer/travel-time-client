bdfb651aae5a4e787098391205d7c2155bf93708
Rebecca Neimeyer <rebeccaneimeyer@RebeccasMacBook.hsd1.ca.comcast.net>
2022-03-29 11:43:42 -0700

    updated method to add new user and verify if user already exist

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Route, Routes, Navigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Home from "./Home/Home";
@@ -8,13 +8,24 @@ import AllTrips from "./AllTrips/AllTrips";
 import About from "./About/About";
 
 function App() {
-  const [user, setUser] = useState("");
+  const urlBase = "http://localhost:4000";
+
+  const [traveller, setTraveller] = useState({name: "", email: ""});
+  const [travellers, setTravellers] = useState([]);
+  const [currentTraveller, setCurrentTraveller] = useState("");
+
+  useEffect(() => {
+    fetch(`${urlBase}/traveller/`)
+    .then((response)=>response.json())
+    .then((data) => setTravellers(data.traveller));
+  }, []);
+
   return (
     <div className="App">
       <Header />
       <main>
         <Routes>
-          <Route path="/" element={<Home user={user} setUser={setUser} />} />
+          <Route path="/" element={<Home urlBase={urlBase} traveller={traveller} setTraveller={setTraveller} travellers={travellers} setTravellers={setTravellers} currentTraveller={currentTraveller} setCurrentTraveller={setCurrentTraveller} />} />
           <Route path="/home" element={<Navigate to="/" />} />
           <Route path="/all-trips" element={<AllTrips />} />
           <Route path="/about" element={<About />} />
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,38 +1,100 @@
 import "./Home.css";
+import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 
-const Home = ({ user, setUser }) => {
+const Home = ({ urlBase, traveller, setTraveller, travellers, setTravellers, currentTraveller, setCurrentTraveller }) => {
+  const [verification, setVerfication] = useState(false);
+  const [signedUp, setSignedUp] = useState(false);
+
+  const handleChange = (event) => {
+    event.persist();
+    setTraveller((prevTraveller) => {
+      const editedTraveller = {
+        ...prevTraveller,
+        [event.target.name]: event.target.value,
+      };
+      return editedTraveller;
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let emails = travellers.map((traveller) => traveller.email);
+    let check = emails.includes(traveller.email);
+    if (check === true) {
+      setVerfication(true);
+    } else {
+      setVerfication(false);
+      fetch(`${urlBase}/traveller/`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify(traveller),
+      })
+        .then(() => fetch(`${urlBase}/traveller/`))
+        .then((response) => response.json())
+        .then((data) => setTravellers(data.traveller))
+        .then(() =>
+          setTraveller({name: "", email: ""})
+        )
+        .then(() => setSignedUp(true));
+    }
+  };
+
   return (
     <div className="home">
-     <h1>Welcome to Travel Time</h1>
-     <h3>We're here to help you plan your next vacation</h3>
-     <h3>Please sign up or log in below to get started!</h3>
-     <div className="form-container">
-         <div>
-     <h2>Sign Up</h2>
-     <Form>
-     <Form.Group>
-            <Form.Label>Name</Form.Label>
-            <Form.Control type="text" placeholder="Your name" />
-        </Form.Group>
-        <Form.Group>
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
-        <Button type="submit">Submit</Button>
-     </Form>
-     </div>
-     <div>
-     <h2>Log In</h2>
-     <Form>
-        <Form.Group>
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
-        <Button type="submit">Submit</Button>
-     </Form>
-     </div>
-     </div>
+      <h1>Welcome to Travel Time</h1>
+      <h3>We're here to help you plan your next vacation</h3>
+      <h3>Please sign up or log in below to get started!</h3>
+      <div className="form-container">
+        <div>
+          <h2>Sign Up</h2>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group>
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Your name"
+                name="name"
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                name="email"
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Button type="submit" onSubmit={handleSubmit}>
+              Submit
+            </Button>
+          </Form>
+        </div>
+        <div>
+          <h2>Log In</h2>
+          <Form>
+            <Form.Group>
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+            <Button type="submit">Submit</Button>
+          </Form>
+        </div>
+      </div>
+      {verification === false ? (
+          <div></div>
+      ) : (
+          <div>That email is already in use. Please sign in!</div>
+      )}
+      {signedUp === false ? (
+          <div></div>
+      ) : (
+        <div>Now please log in!</div>
+      )}
     </div>
   );
 };
