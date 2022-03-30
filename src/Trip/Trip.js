26a112e0804e717f2ab8154f386165dad9c88934
Rebecca Neimeyer <rebeccaneimeyer@RebeccasMacBook.hsd1.ca.comcast.net>
2022-03-30 16:32:54 -0700

    updated trip data to load once defined and updated date format

--- src/Trip/Trip.js
+++ src/Trip/Trip.js
@@ -1,15 +1,35 @@
 import "./Trip.css";
-import { useParams } from "react-router";
-import Flight from "./../Flight/Flight"
-import Hotel from "./../Hotel/Hotel"
+import Flight from "./../Flight/Flight";
+import Hotel from "./../Hotel/Hotel";
 
-const Trip = ({ trip }) => {
-    console.log(trip)
+const Trip = ({ urlBase, currentTrip }) => {
+  console.log(currentTrip);
+
+const cleanDate = (date) => {
+    const d = new Date(date);
+    let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][d.getMonth()];
+    let newDate = month + " " + d.getDate() + ", " + d.getFullYear();
+    return newDate;
+}
+
+let startDate = cleanDate(currentTrip[0].start_date)
+        let endDate = cleanDate(currentTrip[0].end_date)
+
   return (
     <div className="trip">
-     {trip[0].name}
-     <Flight />
-     <Hotel />
+      {currentTrip[0] === undefined ? (
+        <div></div>
+      ) : (
+        <div>
+          <h1 className="trip-name">{currentTrip[0].name}</h1>
+          <h3>Budget: ${currentTrip[0].budget}</h3>
+          <h3>From {startDate} to {endDate}</h3>
+          <h3>Flights</h3>
+          <Flight />
+          <h3>Hotels</h3>
+          <Hotel />
+        </div>
+      )}
     </div>
   );
 };
