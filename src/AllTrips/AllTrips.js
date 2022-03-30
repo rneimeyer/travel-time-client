bb86e5976907e5951b6bd4f0cc9c2b5f96158ac5
Rebecca Neimeyer <rebeccaneimeyer@RebeccasMacBook.hsd1.ca.comcast.net>
2022-03-30 10:54:25 -0700

    udpated map to show user's trips

--- src/AllTrips/AllTrips.js
+++ src/AllTrips/AllTrips.js
@@ -1,6 +1,7 @@
 import "./AllTrips.css";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { Card } from "react-bootstrap";
 
 const AllTrips = ({ urlBase, currentTraveller, travellerId }) => {
 console.log(currentTraveller)
@@ -12,12 +13,30 @@ console.log(currentTraveller)
             .then((data) => setTrips(data.traveller.trips))
     }
 
+    const yourTrips = trips.map((trip) => {
+        return (
+            <div key={trip._id}>
+                <Card>
+                    <Card.Body>
+                        <Card.Title>{trip.name}</Card.Title>
+                        <Card.Text>Budget: {trip.budget}</Card.Text>
+                        <Card.Text>Start Date: {trip.start_date}</Card.Text>
+                        <Card.Text>End Date: {trip.end_date}</Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+        )
+    })
+
     useEffect(() => {
         if(currentTraveller!=="") {
             showTrips();
             console.log("fetch")
+            
         }
     }, [currentTraveller])
+
+
 
   return (
     <div className="all-trips">
@@ -33,7 +52,7 @@ console.log(currentTraveller)
           <div className="current-traveller">
             Current Traveller: {currentTraveller.name}
           </div>
-          <h1>{trips[0].name}</h1>
+          {yourTrips}
         </div>
       )}
     </div>
