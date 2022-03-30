b34aab687fc4a083d0cec7d49d6b014158d278ff
Rebecca Neimeyer <rebeccaneimeyer@RebeccasMacBook.hsd1.ca.comcast.net>
2022-03-30 14:21:18 -0700

    added asset to about page

--- src/About/About.js
+++ src/About/About.js
@@ -1,9 +1,11 @@
 import "./About.css";
+import map from "./../assets/map.png"
 
 const About = () => {
   return (
     <div className="about">
      <h1>About</h1>
+     <img className="about-img" src={map} />
     </div>
   );
 };
