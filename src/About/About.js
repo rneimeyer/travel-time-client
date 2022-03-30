b58ddc154739ae37f4985859a3c49164c106ee6f
Rebecca Neimeyer <rebeccaneimeyer@RebeccasMacBook.hsd1.ca.comcast.net>
2022-03-30 16:31:10 -0700

    added alt to about image

--- src/About/About.js
+++ src/About/About.js
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className="about">
      <h1>About</h1>
-     <img className="about-img" src={map} />
+     <img className="about-img" src={map} alt="overhead view of map to plan travel" />
     </div>
   );
 };
