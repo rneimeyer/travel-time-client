3ad02b1b0adb15ab91190e6f3ebd448966bca0fb
Rebecca Neimeyer <rebeccaneimeyer@RebeccasMacBook.hsd1.ca.comcast.net>
2022-03-30 21:20:20 -0700

    added map to show flight data

--- src/Flight/Flight.js
+++ src/Flight/Flight.js
@@ -1,9 +1,20 @@
 import "./Flight.css";
 
-const Flight = () => {
+const Flight = ({ flights }) => {
+console.log(flights)
+    const yourFlights = flights.map((flight) => {
+        return (
+            <div key={flight._id} className="box">
+                <div>{flight.name}</div>
+                <a href={flight.link} target="_blank">Link to flight</a>
+                <p>Price: ${flight.price}</p>
+            </div>
+        )
+    })
+
   return (
     <div className="flight">
-     Flight
+     {yourFlights}
     </div>
   );
 };
