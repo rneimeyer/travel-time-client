1261b78161046fa9f0e1c859ad7ea1d7410935a2
Rebecca Neimeyer <rebeccaneimeyer@RebeccasMacBook.hsd1.ca.comcast.net>
2022-03-31 10:21:32 -0700

    added current user to header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,12 +2,13 @@ import { Link } from "react-router-dom";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import "./Header.css";
 
-const Header = () => {
+const Header = ({ currentTraveller }) => {
   return (
     <div className="header">
       <Navbar collapseOnSelect bg="white" expand="md" fixed="top">
         <Container>
           <Navbar.Brand className="brand" style={{color: '#F9B9AD'}}>Travel Time</Navbar.Brand>
+          <Navbar.Text>{currentTraveller.name ? ("Welcome, "+currentTraveller.name) : ("")}</Navbar.Text>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
