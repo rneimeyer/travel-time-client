6c933e628de7446b03cb455c24c7fca1e527f926
Rebecca Neimeyer <rebeccaneimeyer@RebeccasMacBook.hsd1.ca.comcast.net>
2022-03-30 21:20:58 -0700

    added map to show hotel data

--- src/Hotel/Hotel.js
+++ src/Hotel/Hotel.js
@@ -1,9 +1,20 @@
 import "./Hotel.css";
 
-const Hotel = () => {
+const Hotel = ({ hotels }) => {
+
+        const yourHotels = hotels.map((hotel) => {
+        return (
+            <div key={hotel._id} className="box">
+                <div>{hotel.name}</div>
+                <a href={hotel.link} target="_blank">Link to hotel</a>
+                <p>Price: ${hotel.price}</p>
+            </div>
+        )
+    })
+
   return (
     <div className="hotel">
-     Hotel
+     {yourHotels}
     </div>
   );
 };
