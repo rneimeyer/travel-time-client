e798f89144664d4593f00a30cc9c9b9a24a46aff
Rebecca Neimeyer <rebeccaneimeyer@RebeccasMacBook.hsd1.ca.comcast.net>
2022-04-01 15:16:45 -0700

    added new class for trip summary and budget status

--- src/Trip/Trip.js
+++ src/Trip/Trip.js
@@ -101,98 +101,103 @@ const Trip = ({ urlBase, currentTripId }) => {
         <div></div>
       ) : (
         <div>
-          <h1 className="trip-name">{currentTrip.name}</h1>
-          <Button onClick={handleShow}>Update Trip</Button>
-          <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>Update Your Trip!</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <Form onSubmit={updateTrip}>
-                <Form.Group>
-                  <Form.Label>Name</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder={currentTrip.name}
-                    name="name"
-                    onChange={handleTripUpdateChange}
-                    required
-                  />
-                </Form.Group>
-                <Form.Group>
-                  <Form.Label>Budget</Form.Label>
-                  <InputGroup>
-                    <InputGroup.Text>$</InputGroup.Text>
+          <div className="trip-summary">
+            <h1 className="trip-name">{currentTrip.name}</h1>
+            <h3>
+              From {cleanDate(currentTrip.start_date)} to{" "}
+              {cleanDate(currentTrip.end_date)}
+            </h3>
+            <Button onClick={handleShow}>Update Trip</Button>
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Update Your Trip!</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Form onSubmit={updateTrip}>
+                  <Form.Group>
+                    <Form.Label>Name</Form.Label>
                     <Form.Control
-                      type="number"
-                      placeholder={currentTrip.budget}
-                      name="budget"
+                      type="text"
+                      placeholder={currentTrip.name}
+                      name="name"
                       onChange={handleTripUpdateChange}
                       required
                     />
-                  </InputGroup>
-                </Form.Group>
-                <Form.Group>
-                  <Form.Label>Start Date</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder={cleanDate(currentTrip.start_date)}
-                    name="start_date"
-                    onChange={handleTripUpdateChange}
-                    required
-                  />
-                </Form.Group>
-                <Form.Group>
-                  <Form.Label>End Date</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder={cleanDate(currentTrip.end_date)}
-                    name="end_date"
-                    onChange={handleTripUpdateChange}
-                    required
-                  />
-                </Form.Group>
-                <Button type="submit">Submit</Button>
-              </Form>
-            </Modal.Body>
-          </Modal>
-          <h3>Budget: ${currentTrip.budget}</h3>
-          <h3>Current Trip Total: ${tripSum}</h3>
-          <h4>{currentTrip.budget>tripSum ? ("You're under budget! Add some more activities!") : ("You're over budget... time to rethink some plans.")}</h4>
-          <h3>
-            From {cleanDate(currentTrip.start_date)} to{" "}
-            {cleanDate(currentTrip.end_date)}
-          </h3>
-          <h3>Flights</h3>
-          <Flight
-            flights={currentTrip.flights}
-            urlBase={urlBase}
-            tripId={currentTrip._id}
-            cleanDate={cleanDate}
-            setRefresh={setRefresh}
-            setFlightSum={setFlightSum}
-            flightSum={flightSum}
-          />
-          <h3>Hotels</h3>
-          <Hotel
-            hotels={currentTrip.hotels}
-            urlBase={urlBase}
-            tripId={currentTrip._id}
-            cleanDate={cleanDate}
-            setRefresh={setRefresh}
-            setHotelSum={setHotelSum}
-            hotelSum={hotelSum}
-          />
-          <h3>Activities</h3>
-          <Activity
-            activities={currentTrip.activities}
-            urlBase={urlBase}
-            tripId={currentTrip._id}
-            cleanDate={cleanDate}
-            setRefresh={setRefresh}
-            setActivitySum={setActivitySum}
-            activitySum={activitySum}
-          />
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Label>Budget</Form.Label>
+                    <InputGroup>
+                      <InputGroup.Text>$</InputGroup.Text>
+                      <Form.Control
+                        type="number"
+                        placeholder={currentTrip.budget}
+                        name="budget"
+                        onChange={handleTripUpdateChange}
+                        required
+                      />
+                    </InputGroup>
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Label>Start Date</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder={cleanDate(currentTrip.start_date)}
+                      name="start_date"
+                      onChange={handleTripUpdateChange}
+                      required
+                    />
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Label>End Date</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder={cleanDate(currentTrip.end_date)}
+                      name="end_date"
+                      onChange={handleTripUpdateChange}
+                      required
+                    />
+                  </Form.Group>
+                  <Button type="submit">Submit</Button>
+                </Form>
+              </Modal.Body>
+            </Modal>
+            <h3>Budget: ${currentTrip.budget}</h3>
+            <h3>Current Trip Total: ${tripSum}</h3>
+            <h4 className="budget-status">
+              {currentTrip.budget > tripSum
+                ? "You're under budget! Plan some more activities!"
+                : "You're over budget... time to rethink some plans."}
+            </h4>
+          </div>
+          <div className="trip-details">
+            <Flight
+              flights={currentTrip.flights}
+              urlBase={urlBase}
+              tripId={currentTrip._id}
+              cleanDate={cleanDate}
+              setRefresh={setRefresh}
+              setFlightSum={setFlightSum}
+              flightSum={flightSum}
+            />
+            <Hotel
+              hotels={currentTrip.hotels}
+              urlBase={urlBase}
+              tripId={currentTrip._id}
+              cleanDate={cleanDate}
+              setRefresh={setRefresh}
+              setHotelSum={setHotelSum}
+              hotelSum={hotelSum}
+            />
+            <Activity
+              activities={currentTrip.activities}
+              urlBase={urlBase}
+              tripId={currentTrip._id}
+              cleanDate={cleanDate}
+              setRefresh={setRefresh}
+              setActivitySum={setActivitySum}
+              activitySum={activitySum}
+            />
+          </div>
         </div>
       )}
     </div>
